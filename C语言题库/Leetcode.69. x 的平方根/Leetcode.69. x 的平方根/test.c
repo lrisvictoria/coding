dfac6f1587958102d9c3081db04c07174ba64034
exploreranduin <a1249967801@163.com>
2022-08-04 10:15:17 +0800

    二分查找

---
+++ C语言题库/Leetcode.69. x 的平方根/Leetcode.69. x 的平方根/test.c
@@ -0,0 +1,35 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1 
+/*
+给你一个非负整数 x ，计算并返回 x 的 算术平方根 。
+
+由于返回类型是整数，结果只保留 整数部分 ，小数部分将被 舍去 。
+
+注意：不允许使用任何内置指数函数和算符，例如 pow(x, 0.5) 或者 x ** 0.5 。
+*/
+
+/*
+输入：x = 8
+输出：2
+解释：8 的算术平方根是 2.82842..., 由于返回类型是整数，小数部分将被舍去。
+*/
+
+int mySqrt(int x)
+{
+	long long left = 0;
+	long long right = 10000000;
+	long long ans = 0;
+	while (left <= right)
+	{
+		long long mid = left + (right - left) / 2;
+		if (mid * mid <= x)
+		{
+			ans = mid;
+			left = mid + 1;
+		}
+		else
+		{
+			right = mid - 1;
+		}
+	}
+	return (int)ans;
+}
