6ef1073f4aa0b920e2cfc02662478f5e36271cb9
exploreranduin <a1249967801@163.com>
2022-10-16 23:47:39 +0800

    10.16 队列实现栈

--- C语言/Leetcode/C语言题库/225. 用队列实现栈/test.c
+++ C语言/Leetcode/C语言题库/225. 用队列实现栈/test.c
@@ -2,7 +2,7 @@
 
 #define _CRT_SECURE_NO_WARNINGS 1 
 
-// 请你仅使用两个栈实现先入先出队列。队列应当支持一般队列支持的所有操作（push、pop、peek、empty）：
+// 请你仅使用两个队列实现一个后入先出（LIFO）的栈，并支持普通栈的全部四种操作（push、top、pop 和 empty）。
 
 #include <stdio.h>
 #include <assert.h>
