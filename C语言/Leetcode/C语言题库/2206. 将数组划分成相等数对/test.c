c22f890a8f0f2ea8a81a9d285307024b47407c79
exploreranduin <a1249967801@163.com>
2022-11-04 15:23:42 +0800

    11.4 哈希表第二练

---
+++ C语言/Leetcode/C语言题库/2206. 将数组划分成相等数对/test.c
@@ -0,0 +1,51 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1 
+
+/*
+给你一个整数数组 nums ，它包含 2 * n 个整数。
+
+你需要将 nums 划分成 n 个数对，满足：
+
+每个元素 只属于一个 数对。
+同一数对中的元素 相等 。
+如果可以将 nums 划分成 n 个数对，请你返回 true ，否则返回 false 。
+
+示例 1：
+
+输入：nums = [3,2,3,2,2,2]
+输出：true
+解释：
+nums 中总共有 6 个元素，所以它们应该被划分成 6 / 2 = 3 个数对。
+nums 可以划分成 (2, 2) ，(3, 3) 和 (2, 2) ，满足所有要求。
+示例 2：
+
+输入：nums = [1,2,3,4]
+输出：false
+解释：
+无法将 nums 划分成 4 / 2 = 2 个数对且满足所有要求。
+ 
+
+提示：
+
+nums.length == 2 * n
+1 <= n <= 500
+1 <= nums[i] <= 500
+
+*/
+
+bool divideArray(int* nums, int numsSize)
+{
+    int hash[501];
+    memset(hash, 0, sizeof(hash));
+    for (int i = 0; i < numsSize; i++)
+    {
+        hash[nums[i]]++;
+    }
+    for (int i = 0; i < numsSize; i++)
+    {
+        if (hash[nums[i]] & 1)
+        {
+            return false;
+        }
+    }
+    return true;
+}
