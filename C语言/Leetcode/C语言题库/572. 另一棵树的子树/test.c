824aa688e79296f09e696a454f9fd0e8c3c85844
exploreranduin <a1249967801@163.com>
2022-11-26 02:11:02 +0800

    11.26 相同的树 + 递归

---
+++ C语言/Leetcode/C语言题库/572. 另一棵树的子树/test.c
@@ -0,0 +1,67 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1 
+
+/*
+给你两棵二叉树 root 和 subRoot 。检验 root 中是否包含和 subRoot 具有相同结构和节点值的子树。如果存在，返回 true ；否则，返回 false 。
+
+二叉树 tree 的一棵子树包括 tree 的某个节点和这个节点的所有后代节点。tree 也可以看做它自身的一棵子树。
+
+ 
+
+示例 1：
+
+
+输入：root = [3,4,5,1,2], subRoot = [4,1,2]
+输出：true
+示例 2：
+
+
+输入：root = [3,4,5,1,2,null,null,null,null,0], subRoot = [4,1,2]
+输出：false
+ 
+
+提示：
+
+root 树上的节点数量范围是 [1, 2000]
+subRoot 树上的节点数量范围是 [1, 1000]
+-104 <= root.val <= 104
+-104 <= subRoot.val <= 104
+
+*/
+
+bool isSameTree(struct TreeNode* p, struct TreeNode* q)
+{
+    if (p == NULL && q == NULL)
+    {
+        return true;
+    }
+
+    if (p == NULL || q == NULL)
+    {
+        return false;
+    }
+
+    if (p->val != q->val)
+    {
+        return false;
+    }
+
+    return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+}
+
+bool isSubtree(struct TreeNode* root, struct TreeNode* subRoot)
+{
+    // 子树不为空，如果树为空，则为假
+    if (root == NULL)
+    {
+        return false;
+    }
+
+    // 如果找到了子树，则直接返回
+    if (isSameTree(root, subRoot))
+    {
+        return true;
+    }
+
+    // 分别递归左右子树，子树只要找到了，则存在
+    return isSubtree(root->left, subRoot) || isSubtree(root->right, subRoot);
+}
