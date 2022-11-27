9e9a92fc2cf2b4c214f2e271db9a65d4b2661be1
exploreranduin <a1249967801@163.com>
2022-11-27 11:59:17 +0800

    11.27 增加 翻转子树判断相等方法

--- C语言/Leetcode/C语言题库/101. 对称二叉树/test.c
+++ C语言/Leetcode/C语言题库/101. 对称二叉树/test.c
@@ -31,14 +31,61 @@
 #include <stdbool.h>
 #include <stdio.h>
 
-bool check(struct TreeNode* p, struct TreeNode* q)
+// 镜像判断
+//bool check(struct TreeNode* p, struct TreeNode* q)
+//{
+//    if (p == NULL && q == NULL)
+//    {
+//        return true;
+//    }
+//
+//    // 一个为空，一个不为空
+//    if (p == NULL || q == NULL)
+//    {
+//        return false;
+//    }
+//
+//    if (p->val != q->val)
+//    {
+//        return false;
+//    }
+//
+//    // 最后递归检查两部分
+//    return check(p->left, q->right) && check(p->right, q->left);
+//}
+//
+//bool isSymmetric(struct TreeNode* root)
+//{
+//    // 检查
+//    return check(root, root);
+//}
+
+// 翻转左子树，左右子树判断是否相等
+
+
+struct TreeNode* invertTree(struct TreeNode* root)
+{
+    if (root == NULL)
+    {
+        return NULL;
+    }
+
+    struct TreeNode* left = invertTree(root->left);
+    struct TreeNode* right = invertTree(root->right);
+
+    root->left = right;
+    root->right = left;
+
+    return root;
+}
+
+bool isSameTree(struct TreeNode* p, struct TreeNode* q)
 {
     if (p == NULL && q == NULL)
     {
         return true;
     }
 
-    // 一个为空，一个不为空
     if (p == NULL || q == NULL)
     {
         return false;
@@ -49,12 +96,31 @@ bool check(struct TreeNode* p, struct TreeNode* q)
         return false;
     }
 
-    // 最后递归检查两部分
-    return check(p->left, q->right) && check(p->right, q->left);
+    return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
 }
-
 bool isSymmetric(struct TreeNode* root)
 {
-    // 检查
-    return check(root, root);
+    // 左右子树都为空
+    if (root->left == NULL && root->right == NULL)
+    {
+        return true;
+    }
+
+    // 左右子树一边不为空
+    if (root->left == NULL || root->right == NULL)
+    {
+        return false;
+    }
+
+    // 左右子树两边都不为空
+    struct TreeNode* left, * right;
+    if (root->left && root->right)
+    {
+        // 翻转左边
+        left = invertTree(root->left);
+        right = root->right;
+    }
+
+    // 和右边子树比较，返回 bool 值
+    return isSameTree(left, right);
 }
