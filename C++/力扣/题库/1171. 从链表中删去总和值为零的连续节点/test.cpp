bb2e20c87afdb95424257295856a7b7db062e1f7
exploreranduin <a1249967801@163.com>
2023-01-03 20:59:07 +0800

    1.3 哈希表 + 前缀和

--- C++/力扣/题库/1171. 从链表中删去总和值为零的连续节点/test.cpp
+++ C++/力扣/题库/1171. 从链表中删去总和值为零的连续节点/test.cpp
@@ -1,8 +1,37 @@
-/*
+﻿/*
+给你一个链表的头节点 head，请你编写代码，反复删去链表中由 总和 值为 0 的连续节点组成的序列，直到不存在这样的序列为止。
+
+删除完毕后，请你返回最终结果链表的头节点。
+
+ 
+
+你可以返回任何满足题目要求的答案。
+
+（注意，下面示例中的所有序列，都是对 ListNode 对象序列化的表示。）
+
+示例 1：
+
+输入：head = [1,2,-3,3,1]
+输出：[3,1]
+提示：答案 [1,2,1] 也是正确的。
+示例 2：
+
+输入：head = [1,2,3,-3,4]
+输出：[1,2,4]
+示例 3：
+
+输入：head = [1,2,3,-3,-2]
+输出：[1]
+ 
+
+提示：
+
+给你的链表中可能有 1 到 1000 个节点。
+对于链表中的每个节点，节点的值：-1000 <= node.val <= 1000.
 
 */
 
-// ��⣺https://leetcode.cn/problems/remove-zero-sum-consecutive-nodes-from-linked-list/solution/ha-xi-biao-qian-zhui-he-by-unruffled-pik-f7ro/
+// 题解：https://leetcode.cn/problems/remove-zero-sum-consecutive-nodes-from-linked-list/solution/ha-xi-biao-qian-zhui-he-by-unruffled-pik-f7ro/
 
 class Solution {
 public:
@@ -11,7 +40,7 @@ public:
         unordered_map<int, ListNode*> hash;
         ListNode* dummy = new ListNode(0, head);
 
-        // ��������ǰ׺��
+        // 对链表求前缀和
         for (ListNode* cur = dummy; cur != nullptr; cur = cur->next) {
             sum += cur->val;
             hash[sum] = cur;
@@ -19,9 +48,9 @@ public:
 
         sum = 0;
         for (ListNode* cur = dummy; cur != nullptr; cur = cur->next) {
-            // �����ǰ�ڵ��ǰ׺�͵���ĳλ��ǰ׺�͵�ֵ��˵�� (cur, hash[sum] ] ���䶼��Ҫ��ɾ����
-            // ��������Ϳ��԰� cur->next = hash[sum]->next
-            // �����м�Ľڵ㣬��ɾ��
+            // 如果当前节点的前缀和等于某位置前缀和的值，说明 (cur, hash[sum] ] 区间都是要被删除的
+            // 这种情况就可以把 cur->next = hash[sum]->next
+            // 跳过中间的节点，即删除
             sum += cur->val;
             cur->next = hash[sum]->next;
         }
